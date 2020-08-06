7a438d387d183ad4075e3aaeedb3ed280eaae614
FranDallasta <dallastafranco@gmail.com>
2020-08-06 20:09:03 -0300

    actualizado 06/08

--- EjercicioParcialito1E-master/EjercicioParcialito1E-master/dos.js
+++ EjercicioParcialito1E-master/EjercicioParcialito1E-master/dos.js
@@ -10,24 +10,45 @@ Al finalizar, se debe informar:
 Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
 */
 
-function mostrar()
-{
+function mostrar() {
 	respuesta = "si";
 	let nombreArt;
 	let categoriaArt;
 	let precioArt;
 	let mayorPrecio = 0;
+	let nombreLacteo;
+	let precioLacteo;
+	let cantidadBebidas = 0;
+	let nombreMayor;
+	let precioMayorLacteo = 0;
 
 	respuesta = prompt("Quiere ingresar un producto ?");
 	while (respuesta == "si") {
-		precioArt = parseInt(prompt ("Precio del articulo"));
-		if(mayorPrecio < precioArt)
-		{
-			nombreArt = prompt ("Nombre del articulo");
+		nombreArt = prompt("Nombre del articulo");
+		do {
 			categoriaArt = prompt("Catergoria del articulo");
+		} while (!(categoriaArt == "lacteo" || categoriaArt == "bebidas" || categoriaArt == "almacen" || categoriaArt == "limpieza"));
+		do {
+			precioArt = parseInt(prompt("Precio del articulo"));
+		} while (!(precioArt <= 1000 && precioArt >= 0));
+		if (mayorPrecio < precioArt) {
+			nombreMayor = nombreArt;
+			mayorPrecio = precioArt;
+		}
+		if (categoriaArt == "lacteo") {
+			if (precioMayorLacteo < precioArt) {
+				nombreLacteo = nombreArt;
+				precioMayorLacteo = precioArt;
+			}
+		}
+		if (categoriaArt == "bebidas") {
+			cantidadBebidas++;
 		}
 		respuesta = prompt("Quiere ingresar otro articulo?");
 	}
-
-	alert(`El mayor precio es de $${precioArt} y corresponde un(a) ${nombreArt} en la categoria ${categoriaArt}`);
+	document.writeln(`El mayor precio es de $${mayorPrecio} y corresponde un(a) ${nombreMayor}` + "<br>");
+	document.writeln(`La cantidad de bebidas es de ${cantidadBebidas}` + "<br>");
+	document.writeln(`El prodcuto lacteo ${nombreLacteo} vale $${precioMayorLacteo}` + "<br>");
 }
+
+
